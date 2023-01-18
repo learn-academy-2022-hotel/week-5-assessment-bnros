efade5d22bdaf963fc2d70da501ffb307e0f4d11
bnros <b.ros92@gmail.com>
2023-01-18 08:53:42 -0800

    completed week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,7 +12,29 @@
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
+// PSEUDOCODE:
+// input: a string that takes in a word
+// output: a new string with the word converted into a new word
+
+// process:
+// delcare a function called "wordDecoder"
+// after declaring function, give it the parameter of "string"
+// create a for loop to iterate each letter
+// referring to the resource, using switch statements, create a case for each letter aeio, to convert to 4310
+// create an additional switch statement to account for the capitalized letter of "E" to also convert to 3
+
+// references/resources:
+// https://www.w3schools.com/js/js_switch.asp
 // a) Create a test with expect statements using the variables provided.
+
+// RED:
+describe("wordDecoder", () => {
+    it("takes a string and decodes the message", () =>{
+        expect(wordDecoder(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect(wordDecoder(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(wordDecoder(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
 
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
@@ -23,9 +45,55 @@ const secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+// GREEN:
+const wordDecoder = (string) => {
+    let letterDecoded = ""
+    for (let i = 0; i < string.length; i++) {
+        switch (string[i]) {
+            case "a":
+                letterDecoded += "4"
+                break
+            case "e":
+                letterDecoded += "3"
+                break
+            case "i":
+                letterDecoded += "1"
+                break
+            case "o":
+                letterDecoded += "0"
+                break
+            case "E":
+                letterDecoded += "3"
+                break
+            default:
+                letterDecoded += string[i]
+        }
+    }
+    return letterDecoded
+}
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
+// PSEUDOCODE:
+// input: an array of words 
+// output: a new array of words returned with all the words containing a specific, single letter
+
+// process:
+// declare a function called "containsLetter"
+// after declaring function, pass it two parameters to account for the array being passed in, and the specific letter. I used "array" and "letter"
+// return the statement using the .filter() method to return a new array with specifications to include only words where that letter appear
+// account for upper cased letters by using .toLowerCase() method, then use .include() method to specify letter variable
+
+
 // a) Create a test with expects statement using the variable provided.
+
+// RED:
+describe("containsLetter", () => {
+    it("takes a letter and returns an array with all words containing specific letter", () => {
+        expect(containsLetter(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(containsLetter(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -36,9 +104,39 @@ const letterE = "e"
 
 // b) Create the function that makes the test pass.
 
+// GREEN:
+const containsLetter = (array, letter) => {
+ return array.filter((array) => array.toLowerCase().includes(letter))
+}
+
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
+// PSEUDOCODE:
+// input: an array of five numbers
+// output: a boolean value of true or false, letting us know if the given array is a full house or not
+
+// process:
+// declare a variable called "fullHouse"
+// after declaring variable, pass it the parameter of "numbers"
+// create an empty variable called "numCounts" to take in value of for loop
+// create a for loop to cycle through given number in the array
+// create two new variables called "twoOfAKind" and "threeofAKind" and set to false
+// create another for loop to cycle and see if those numbers strictly equal 3 or 2, if yes, set those variables to true
+// return the statement telling it to only return if the boolean value "true" for both variables of "twoOfAKind" and "threeOfAKind" using the && operator to account for both conditions
+
+// references/resources:
+// https://stackoverflow.com/questions/59510349/how-to-recognise-full-house-and-two-pair-in-a-javascript-poker-game
+
 // a) Create a test with expect statements using the variable provided.
+// RED:
+describe("fullHouse", () => {
+    it("takes in five numbers and determines if there is a full house", () =>{
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+        expect(fullHouse(hand4)).toEqual(true)
+    })
+})
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -50,3 +148,25 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+
+//GREEN:
+const fullHouse = (numbers) => {
+    let numCounts = {}
+    for (let i = 0; i < numbers.length; i++) {
+        if (numCounts[numbers[i]]) {
+            numCounts[numbers[i]]++
+        } else {
+            numCounts[numbers[i]] = 1
+        }
+    }
+    let threeOfAKind = false;
+    let twoOfAKind = false;
+    for (let num in numCounts) {
+      if (numCounts[num] === 3) {
+        threeOfAKind = true;
+      } else if (numCounts[num] === 2) {
+        twoOfAKind = true;
+      }
+    }
+    return threeOfAKind && twoOfAKind;
+  }
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,47 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: When calling a super in a class, it allows the child class to inherit common behaviors from the parent class. This is designated using super().
 
-Researched answer:
+Researched answer: In Ruby, when calling the super method inside a class, it invokes the same methods that are found in the parent class of the current class. This in turn allows the sublass to inherit full functionality or partial functionality of the parent class. Additionally, it also allows you to modify or add to the new subclass.
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem is a type of testing dependencies that are required to be installed when testing Ruby code with RSpec.
 
-Researched answer:
+Researched answer: A Gem in Ruby refers to a package manager module that you can install and use on every project. Additionally, a gem is a self-contained format that contains a standard format
+to distribute many different types of programs and libraries within the Ruby language. The "gem" command itself is called to build, download, upload, and install these Gem packages. This is useful because it allows us because by using gems, it allows us to extend the functionality of Ruby by implementing additional code, functionalities, and other frameworks that are not typically found in the standard Ruby library.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: Unfortunately, I cannot recall wheat a relational database is.
 
-Researched answer:
+Researched answer: Relational databases refers programs that are used for the purpose of creating, updating, and managing relational databases. Relational databases are useful because it allows organization of data into tables and columns, similarly to what you would find in a spreadsheet. The data found within the columns and tables can then be related to each other by using primary keys. The most popular ones are PostgreSQL, MySQL, MariaDB, Microsoft SQL Server, and Oracle Database. Additionally, asides fromr relational databases, a few other databases that exists are:
+- NoSQL databases - databases that do not use the relational model, but instead opt to use flexible schema
+-graph databases - as the name implies, used to store and query data that is then organized into a graph
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: Primary keys are special, key words that allow relational databases to relate the data within columns and tables together. 
 
-Researched answer:
+Researched answer: Primary keys are unique identitifers that aid in identifying and finding a specific row, or record within a given relational database table. There are two different types of primary keys: natural key, and surrogate key Primary keys are important because they allow relationships between tables to be defined, and they are structured within a column, or sets of columns and can be used to identify each row within a table, without returning any duplicate values that may appear.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: HTTP verbs are Get, Post, Delete, and I can't remember what the other ones are. Get corresponds to the R-read in CRUD, Post refers to C-create in CRUD, and Delete corresponds to the D-delete in the CRUD model
 
-Researched answer:
+Researched answer: TheThe 5 HTTP verbs, also known as the HTTP methods refer to Get, Post, Put, Patch, and Delete. Respectively, they correspond to the developer's philosophy (CRUD action's) of R-ead, C-reate, U-pdate, U-pdate, and D-elete. These verbs tells the server what to do with the data being identified.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: Stands for Representational State Transfer, RESTful routes are a way to organize, clean, consistent URL paths by implementing HTTP verbs and CRUD actions
 
-2. Model validations:
+2. Model validations: Model validation refers to the process of checking that a given data model, or a set of data models, meets certain requirements or constraints before it is used. The purpose of this is to ensure that the data is correct, consistent and complete.
 
-3. Params:
+3. Params: Short for parameters, a param is a value that is passed onto a function, or method, or in order to control its behavior or to specify the input it should operate on
 
-4. ERB:
+4. ERB: ERB stands for Embeded Ruby, refers to a templating system that allows the programmer to embed Ruby code inside of plain text files. Typically used in frameswork such as Ruby on Rails
 
-5. API:
+5. API: API stands for Application Program Interface, and refers to a set of rules or protocols and specific tools that should be used when building sofrware and applications.
